e89e28118c843a80723bf4a4cf74021175a75fb1
Vitor Eiti Uekawa <vuekawa@Vitors-MacBook-Air.local>
2022-12-02 12:24:18 +0900

    Removing unrelated files

--- untitled.m
+++
@@ -1,8 +0,0 @@
-syms x1 x2 a 
-f = [x2 - (x1^3 - a * x1 * x2^2) * (x1^2 + 2*x2^2);
-     -2 * x1 + (6* a * x1^2 * x2 - 8 * x2^3) * (x1^2 + 2 * x2^2)];
-
-v_fun = x1^2 + x2^2;
-
-v_dot = transpose(gradient(v_fun)) * f;
-
