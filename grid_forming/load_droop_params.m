801652dc5b2dcc52f2da1e2a7848b391c8714dbc
vitoruekawa <vitor_eiti@hotmail.com>
2023-08-15 14:34:06 +0900

    rename load_gfmi_params to load_droop_params

--- grid_forming/load_droop_params.m
+++ grid_forming/load_droop_params.m
@@ -24,9 +24,12 @@ L_f = (1/n) * 200 * 1e-6 / Lr * (Vr^2 / Vb^2) * (Sb / Sr);
 C_f = n * 300 * 1e-6 / Cr * (Vr^2 / Vb^2) * (Sb / Sr);
 R_dc = 1.2 / Zr * (Vr^2 / Vb^2) * (Sb / Sr); % Why isn't this term multiplied by n?
 C_dc = n * 0.008 / Cr * (Vr^2 / Vb^2) * (Sb / Sr);
-vdc_st = 2.44 * 1e+3;
+vdc_st = 2.44 * 1e+3 / Vb;
 tau_dc = 0.05;
 idc_max = 1.2 * Ir / Ib; % Why isn't this term multiplied by n?
+
+R_g = 0;
+L_g = L_f * 3;
 
 %% AC and DC current and voltage control
 Kp_v = 0.52;
@@ -41,7 +44,7 @@ Kp = 0.001;
 Ki = 0.5;
 
 %% Store parameters in tables
-vsc_params = table(C_dc, R_dc, L_f, R_f, C_f);
+vsc_params = table(C_dc, R_dc, L_f, R_f, C_f, R_g, L_g);
 dc_source_params = table(vdc_st, idc_max, tau_dc, k_dc, R_dc);
 controller_params = table(L_f, C_f, R_f, Kp_v, Ki_v, Kp_i, Ki_i, vdc_st);
 ref_model_params = table(omega_st, d_w, Kp, Ki);
