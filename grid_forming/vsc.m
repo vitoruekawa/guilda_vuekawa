b7f1b4da1cf4aced1ef091d2e5b4197c4aab67fd
vitoruekawa <vitor_eiti@hotmail.com>
2023-08-17 14:32:42 +0900

    considering constant ideal dc source

--- grid_forming/vsc.m
+++ grid_forming/vsc.m
@@ -23,15 +23,14 @@ classdef vsc < handle
         end
 
         function nx = get_nx(obj)
-            nx = 7;
+            nx = 6;
         end
 
         function nu = get_nu(obj)
             nu = 0;
         end
 
-        function [d_vdc, d_isdq, d_vdq, d_idq] = get_dx(obj, idc, vdc, ix, isdq, idq, omega, vdq, vsdq, Vdq)
-            d_vdc = (idc - (vdc / obj.R_dc) - ix) / obj.C_dc;
+        function [d_isdq, d_vdq, d_idq] = get_dx(obj, isdq, idq, omega, vdq, vsdq, Vdq)
             d_isdq = (-(obj.R_f * eye(2) + omega * obj.L_f * [0, -1; 1, 0]) * isdq - vdq + vsdq) / obj.L_f;
             d_vdq =  (-obj.C_f * omega * [0, -1; 1, 0] * vdq + isdq - idq) / obj.C_f;
             d_idq = (-(obj.R_g * eye(2) + omega * obj.L_g * [0, -1; 1, 0]) * idq - Vdq + vdq) / obj.L_g;
