7fa50b7f2e55ec1e638742392387199b9e8a0947
vitoruekawa <vitor_eiti@hotmail.com>
2023-11-06 01:13:51 +0900

    first version of gfmi_vsm_1axis (still with bugs)

---
+++ grid_forming/vsm_1axis.m
@@ -0,0 +1,62 @@
+classdef vsm_1axis < handle
+
+    properties
+        omega_st
+        Xd
+        Xd_prime
+        Xq
+        Tdo
+        M
+        D
+        P_st
+        V_st
+    end
+
+    methods
+
+        function obj = vsm_1axis(vsm_params)
+            obj.omega_st = vsm_params{:, 'omega_st'};
+            obj.Xd = vsm_params{:, 'Xd'};
+            obj.Xd_prime = vsm_params{:, 'Xd_prime'};
+            obj.Xq = vsm_params{:, 'Xq'};
+            obj.Tdo = vsm_params{:, 'Tdo'};
+            obj.M = vsm_params{:, 'M'};
+            obj.D = vsm_params{:, 'D'};
+        end
+
+        function nx = get_nx(obj)
+            nx = 3;
+        end
+
+        function nu = get_nu(obj)
+            nu = 0;
+        end
+
+        % State variables: theta and zeta (PI controller)
+        function Efd = get_Efd(obj, Eq, Vq)
+            Efd = obj.Xd * Eq / obj.Xd_prime - (obj.Xd / obj.Xd_prime - 1) * Vq;
+        end
+
+        function [d_delta, d_omega, d_Eq] = get_dx(obj, P, Pmech, Vfd, omega, Efd)
+            d_delta = obj.omega_st * omega;
+            d_omega = (- obj.D * omega + Pmech - P) / obj.M;
+            d_Eq = (-Efd + Vfd) / obj.Tdo;
+        end
+
+        function [delta_st, domega_st, Eq_st, Vfd] = get_equilibrium(obj, P, Q, Vabs, Vangle)
+
+            delta_st = Vangle + atan((P * obj.Xq) / (Vabs ^ 2 + Q * obj.Xq));
+
+            domega_st = 0;
+
+            Enum = Vabs ^ 4 + Q ^ 2 * obj.Xd_prime * obj.Xq + Q * Vabs ^ 2 * obj.Xd_prime + Q * Vabs ^ 2 * obj.Xq + P ^ 2 * obj.Xd_prime * obj.Xq;
+            Eden = Vabs * sqrt(P ^ 2 * obj.Xq ^ 2 + Q ^ 2 * obj.Xq ^ 2 + 2 * Q * Vabs ^ 2 * obj.Xq + Vabs ^ 4);
+            Eq_st = Enum / Eden;
+
+            Vfd = obj.Xd * Eq_st / obj.Xd_prime - (obj.Xd / obj.Xd_prime - 1) * Vabs * cos(delta_st - Vangle);
+
+        end
+
+    end
+
+end
